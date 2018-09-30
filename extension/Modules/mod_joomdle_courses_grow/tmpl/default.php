41f77e4517988f32c8643be02806e3840e5f5f65
ahmad22as <ahmad22as@yahoo.com>
2018-09-30 16:43:58 +0330

    • به روز رسانی ماژول mod_joomdle_courses_grow نسخه شخصی 5 به نسخه شخصی 5 تیپ 2
      5 تیپ 2 :
        - تغییر نحوه نمایش اطلاعات درس

--- extension/Modules/mod_joomdle_courses_grow/tmpl/default.php
+++ extension/Modules/mod_joomdle_courses_grow/tmpl/default.php
@@ -92,7 +92,7 @@
 
                     /* COURSE TITLE SECTION */
                     echo '<div class="corstitle',$curso['cat_id'],' corstitle">';
-                        echo '<p class="joomdle_course_columns_titr" style="">';
+                        echo '<p class="joomdle_course_columns_titr" style="float: left; padding-left: 8px;">';
                             if ($linkto == 'moodle') {
                                 if ($default_itemid)
                                     $itemid = $default_itemid;
@@ -117,6 +117,21 @@
                         echo '</p>';
                     echo '</div>';
 
+                    /* TEACHER NAME SECTION */
+                    echo '<div class="profnme' . $course_info['cat_id'] . ' profnme" >';
+                        if ($haveTeacher==true){
+                            if ($teacher['username'])
+                                $teacherLink = JRoute::_("index.php?option=com_joomdle&view=teacher&username=".$teacher['username']."&Itemid=$itemid");
+                            if ($teacher['lastname'])
+                                $teacherPresentation = "مدرس : استاد " . $teacher['lastname'];
+                        }
+                        else {
+                            $teacherLink = "#";
+                            $teacherPresentation = "مدرس : نامشخص";
+                        }
+                        echo '<a href="' . $teacherLink . '">' . $teacherPresentation . '</a>';
+                    echo '</div>';
+
                     /* COURSE SUMMARY SECTION */
                     if ($curso['summary']) {
                         $curso_summary = trim(strip_tags($curso['summary']));
@@ -134,21 +149,6 @@
                             echo "<a style=\"direction:rtl\" "."href=\"".$url."\">[اطلاعات بیشتر]</a><br>";
                         echo '</div>';
                     }
-
-                    /* TEACHER NAME SECTION */
-                    echo '<div class="profnme' . $course_info['cat_id'] . ' profnme" >';
-                        if ($haveTeacher==true){
-                            if ($teacher['username'])
-                                $teacherLink = JRoute::_("index.php?option=com_joomdle&view=teacher&username=".$teacher['username']."&Itemid=$itemid");
-                            if ($teacher['lastname'])
-                                $teacherPresentation = "مدرس : استاد " . $teacher['lastname'];
-                        }
-                        else {
-                            $teacherLink = "#";
-                            $teacherPresentation = "مدرس : نامشخص";
-                        }
-                        echo '<a href="' . $teacherLink . '">' . $teacherPresentation . '</a>';
-                    echo '</div>';
 
                     /* COURSE ENROLL BUTTON SECTION */
                     echo '<div class="inlineForm center" style="padding:10px" >';
@@ -220,48 +220,18 @@
             $('.owl-carousel').slick({
                 infinite: true,
                 rtl: true,
-                slidesToShow: 6,
+                slidesToShow: 3,
                 slidesToScroll: 1,
-                autoplay: true,
+                // autoplay: true,
                 autoplaySpeed: 3000,
                 adaptiveHeight: true,
                 // nextArrow: '<button type="button" class="slick-next fa fa-chevron-circle-left fa-3x" aria-label="Next">Next</button>',
                 // prevArrow: '<button type="button" class="slick-prev fa fa-chevron-circle-right fa-3x" aria-label="Previous">Previous</button>',
                 dots: true,
                 responsive: [{
-                    breakpoint: 2000,
-                    settings: {
-                        slidesToShow: 6,
-                        slidesToScroll: 1
-                    }
-                },{
-                    breakpoint: 1500,
-                    settings: {
-                        slidesToShow: 5,
-                        slidesToScroll: 1
-                    }
-                },{
-                    breakpoint: 1300,
-                    settings: {
-                        slidesToShow: 4,
-                        slidesToScroll: 1
-                    }
-                },{
-                    breakpoint: 1024,
-                    settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 1
-                    }
-                },{
-                    breakpoint: 800,
+                    breakpoint: 950,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 1
-                    }
-                },{
-                    breakpoint: 500,
-                    settings: {
-                        slidesToShow: 1,
                         slidesToScroll: 1
                     }
                 }]
